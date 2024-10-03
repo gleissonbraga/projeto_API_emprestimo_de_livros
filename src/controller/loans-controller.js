ef395702fa1d76f4e1bec79ee3288586ab68a848
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-10-02 22:58:19 -0300

    Finalizando projeto

--- src/controller/loans-controller.js
+++ src/controller/loans-controller.js
@@ -1,5 +1,6 @@
 const HttpError = require("../errors/HttpError")
 const loansModel = require("../middlewares/loans-model")
+const booksModel = require("../models/books-model")
 
 module.exports = {
     // GET /api/loans
@@ -13,12 +14,30 @@ module.exports = {
         const { id } = req.params
         const loan = loansModel.getAllLoanById(id)
         if (!loan) throw new HttpError(404, 'Empréstimo não encontrado')
-            res.json(loan)
-    }
+        res.json(loan)
+    },
 
     // POST /api/loans
+    save: (req, res) => {
+        const user = req.user
+        const { bookId } = req.body
+
+        if(typeof bookId !== 'string') throw new HttpError(400, 'Id do livro inválido!')
+
+        const book = booksModel.getAllLoanById(bookId)
+        if(!book) throw new HttpError(404, 'Livro não encontrado!')
+
+        const newLoan = loansModel.createLoan(user, book)
+        res.status(201).json(newLoan)
+    },
 
     // POST /api/loans/:id/return
+    return: (req, res) => {
+        const { id } = req.params
+
+        const loan = loansModel.returnLoan(id)
+        res.json(loan)
+    }
 }
 
 
